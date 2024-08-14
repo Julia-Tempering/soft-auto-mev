2e24b995f4ef1e58d7a9f8e367083ac1a5103340
Miguel Biron-Lattes <miguelbl9@gmail.com>
2024-08-14 10:48:52 -0700

    online estimator

--- julia-environment/src/online_ess.jl
+++ julia-environment/src/online_ess.jl
@@ -1,5 +1,57 @@
-include("instantiate_env.jl")
-include("utils.jl")
+include("batch_means.jl")
+using OnlineStatsBase
+using Random
+
+#=
+An online exact (i.e., known mean and sd) ESS estimator based on truncated
+spectral density at zero (i.e., truncated integrated autocorrelation time).
+
+TODO: results are too dependent on the autocorrelation index cutoff K.
+=#
+struct ExactESS{TF<:Real,TCB<:CircBuff,TACS} <: OnlineStat{TF}
+    μ::TF
+    σ::TF
+    buffer::TCB
+    acs::TACS
+    function ExactESS(μ::TF, σ::TF, buffer::CircBuff{TF}, acs::NTuple) where {TF}
+        @assert length(buffer.rng.rng) == length(acs)
+        new{TF,typeof(buffer),typeof(acs)}(μ, σ, buffer, acs)
+    end
+end
+function ExactESS(μ::Real, σ::Real; K::Integer = 256)
+    TF = float(promote_type(typeof(μ), typeof(σ)))
+    buffer = CircBuff(TF, K)
+    acs = ntuple(i -> Mean(TF), K)
+    ExactESS(TF(μ), TF(σ), buffer, acs)
+end
+OnlineStatsBase.nobs(e::ExactESS) = nobs(e.buffer)
+std_spectrum_at_zero(e::ExactESS) = max(zero(e.μ), one(e.σ) + 2sum(value(e.buffer)))
+spectrum_at_zero(e::ExactESS) = abs2(e.σ) * std_spectrum_at_zero(e)
+relative_ESS(e::ExactESS) = inv(std_spectrum_at_zero(e))
+OnlineStatsBase.value(e::ExactESS) = nobs(e) * relative_ESS(e)
+function OnlineStatsBase._fit!(e::ExactESS, y)
+    z = (y-e.μ)/e.σ                     # standardize observation
+    k = length(e.buffer)                # number of available elements in the buffer
+    for i in 1:k
+        fit!(e.acs[i], e.buffer[i] * z) # update the i-th autocorrelation estimator
+    end
+    fit!(e.buffer, z)                   # add standardized obs to the circular buffer
+end
+
+
+# test
+vals = randn(100000)
+# vals = range(0.0,5.0,length=100000)
+e = ExactESS(0, 1; K=4)
+fit!(e, vals)
+value.(e.acs)
+std_spectrum_at_zero(e)
+batch_means_ess(vals,0.0,1.0)
+
+
+#=
+A standardized (σ=1) AR1 process for testing the estimator
+=#
 
 struct StandardAR1Process
     ϕ::Float64
@@ -13,17 +65,14 @@ function simulate(a::StandardAR1Process, rng::AbstractRNG, n::Integer)
     Iterators.flatten((x0, tail))
 end
 
-rng = SplittableRandom(1)
+rng = Xoshiro(1)
 a = StandardAR1Process(0.9)
 spectrum_at_zero(a)
-n = 9
+n = 100_000
 res = simulate(a, rng, n)
-var(res)
-asymptotic_var(a)
-v = collect(res)
-batch_means_ess(v, 0.0, sqrt(asymptotic_var(a)))/n
-batch_means_ess(v)/n
-
-k = 3
-s = Iterators.Stateful(simulate(a, SplittableRandom(1), n))
-collect(Iterators.take(s, k))
+e = ExactESS(0, sqrt(asymptotic_var(a)); K=63)
+nobs(e)
+fit!(e,res)
+relative_ESS(e)
+relative_ESS(a)
+value.(e.acs)
